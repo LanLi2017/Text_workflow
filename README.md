6ff0c038edb2a4fea60388e9937aa7c5ef02c22b
LanLi2017 <lilan.scut@gmail.com>
2018-02-22 23:34:55 -0600

    1 update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Text_workflow
+1. Using MeTA tookit to accomplish the text-mining process
+2. construct relationship between MeTA with the YesWorkflow
+3. construct relationship between CWL(Common Workflow Language)
+4. compare these two 
---
+++ test.py
@@ -0,0 +1,66 @@
+import metapy
+import matplotlib.pyplot as plt
+
+"""
+@begin parameter_search @desc Workflow for using ranking function to score the parameter
+@param s @desc the parameter s of the ranking function PivotedLength
+@in config.toml file @desc metapy configuration file to do text mining
+@out score @desc the final score for ranking function
+@out avg_p @desc average precision for every query
+@out test.png @uri file: test.png 
+"""
+
+def parameter_search(s):
+    """
+    @begin metapy_run @desc Create metapy idx and queries and using IREval from config files
+    @in  config_file @uri file: config.toml
+    @out idx @desc make inverted index
+    @out query @desc build the query object
+    @out ev   @desc to do an IR evaluation
+    """
+    idx=metapy.index.make_inverted_index('config.toml')
+    query=metapy.index.Document()
+    ev=metapy.index.IREval('config.toml')
+
+    """
+    @end metapy_run
+    """
+
+    """
+    @begin score_calculate @desc calculate scores for queries and plot
+    @param num_results @desc loop over the queries file and add each result to the IREval object ev
+    @param s
+    @in idx 
+    @in ev 
+    @out score 
+    @out avg_p 
+    @out test.png 
+    """
+    num_results=10
+    # list_s=[]
+    score=[]
+    for si in range(len(s)):
+        # list_s.append(s)
+        ranker=metapy.index.PivotedLength(si)
+        with open('cranfield-queries.txt')as query_file:
+            for query_num,line in enumerate(query_file):
+                query.content(line.strip())
+                results=ranker.score(idx,query,num_results)
+                avg_p=ev.avg_p(results,query_num,num_results)
+                print(avg_p)
+        score.append(ev.map())
+    plt.plot(s,score,'ro')
+    plt.savefig('test.png')
+    """
+    @end score_calculate
+    """
+
+"""
+@end parameter_search()
+"""
+
+
+
+if __name__ == '__main__':
+    parameter_search(list(range(10)))
+
